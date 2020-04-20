81edfcf2dd4aa7959deceadbbcc4fe2d1ffcda8b
JovanStef <jov.box@outlook.com>
2020-04-20 13:57:36 +0200

    front end with moustache

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,78 @@
 const socket = io();
+
+//Element
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormBtn = $messageForm.querySelector('button');
+const $locationBtn = document.querySelector('#location-btn');
+const $messages = document.querySelector('#messages');
+
+
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationTemplate = document.querySelector('#location-template').innerHTML;
+
+
 //receive
 socket.on('message' , (message)=>{
-    console.log(message)
+    console.log(message);
+    const html = Mustache.render(messageTemplate, {
+        message:message.text,
+        createdAt:moment(message.createdAt).format('hh:mm:ss a')
+    });
+    $messages.insertAdjacentHTML('beforeend', html); 
 });
 
-document.querySelector('#message-form').addEventListener('submit', (e)=>{
+socket.on('locationMessage', (url)=>{
+    console.log(url)
+    const html = Mustache.render(locationTemplate , {
+        url:url.url,
+        createdAt:moment(url.createdAt).format('hh:mm:ss a')
+    });
+    $messages.insertAdjacentHTML('beforeend' , html);
+});
+
+$messageForm.addEventListener('submit', (e)=>{
     e.preventDefault();
+
+    //disable button
+    $messageFormBtn.setAttribute('disabled', 'disabled');
 
     // let message = document.querySelector('input').value;
     let message = e.target.elements.message.value
 
-    //send
-    socket.emit('sendMessage' , message)
+    //send message
+    socket.emit('sendMessage' , message, (err)=>{
+    
+    //enable button
+    $messageFormBtn.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
+
+        if(err){
+            return console.log(err)
+        }
+        console.log('Delivered')
+
+    });
+});
+
+$locationBtn.addEventListener('click', ()=>{
+    if(!navigator.geolocation){
+        return alert('Geolocation not supported on your browser')
+    }
+    //disable button
+    $locationBtn.setAttribute('disabled' , 'disabled');
+
+        navigator.geolocation.getCurrentPosition((position)=>{
+          socket.emit('sendLocation', {
+              latitude:position.coords.latitude,
+              longitude:position.coords.longitude
+          } , ()=>{
+              // enable button
+              $locationBtn.removeAttribute('disabled');
+
+              console.log('Location shared')
+            });
+        });
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
+const{generateMessage , generateLocationMessage}=require('./utils/messages')
 
 const app = express();
 const server = http.createServer(app);
@@ -17,20 +19,32 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection' , (socket)=>{
     console.log('New WebSocket connection');
 //send
-    socket.emit('message' , 'Welcome');
+    socket.emit('message' , generateMessage('Welcome'));
 
     //broadcast to all
-    socket.broadcast.emit('message' , 'A new user has joined');
+    socket.broadcast.emit('message' , generateMessage('A new user has joined'));
 
     //receive
-    socket.on('sendMessage' , (message)=>{
+    socket.on('sendMessage' , (message , callback)=>{
+        const filter = new Filter;
+
+        if(filter.isProfane(message)){
+            return callback('Profanity is not allowed')
+        }
         //send
-        io.emit('message' , message)
+        io.emit('message' , generateMessage(message));
+        callback('Delivered');
     });
+
+    //send location
+    socket.on('sendLocation', (coords , callback)=>{
+        io.emit('locationMessage' , generateLocationMessage(`https://www.google.com/maps?q=${coords.latitude},${coords.longitude}`));
+        callback()
+    })
 
     //disconnect
     socket.on('disconnect' , ()=>{
-        io.emit('message' , 'A user has left')
+        io.emit('message' , generateMessage('A user has left'))
     });
 
 });
